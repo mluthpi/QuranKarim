379c3d0bc977a0f55c4914e639b7ddcdbe582577
muhammad Luthfiyanto <luthfiyantomuhammad@gmail.com>
2023-07-13 21:55:09 +0700

    try add splashscreen

--- app/src/main/java/com/example/qurankarim/MainActivity.kt
+++ app/src/main/java/com/example/qurankarim/MainActivity.kt
@@ -1,46 +1,84 @@
 package com.example.qurankarim
 
+import android.media.Image
 import android.os.Bundle
+import android.window.SplashScreen
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.semantics.Role.Companion.Image
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.example.qurankarim.ui.theme.QuranKarimTheme
+import kotlinx.coroutines.delay
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            QuranKarimTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+
+
+        }
+    }
+}
+
+@Composable
+fun navigation() {
+
+    val navController = rememberNavController()
+    NavHost(
+        navController = navController,
+        startDestination = "splash_screen"
+    ){
+        composable("splash_screen") {
+            SplashScreen(navController = navController)
+        }
+
+        composable("main_screen") {
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun SplashScreen(navController: NavController){
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    QuranKarimTheme {
-        Greeting("Android")
+    LaunchedEffect(key1 = true){
+        delay(5000L)
+        navController.navigate("main_screen")
+    }
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.TopCenter,
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.ic_background),
+            contentDescription = "",
+            modifier = Modifier.width(200.dp))
     }
 }
+
